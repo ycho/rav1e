ba1a7839d217e0d61bbc12a888b1009b49f9b1b3
Yushin Cho <ycho@mozilla.com>
2018-09-14 12:40:51 -0700

    Added asseerts

--- src/encoder.rs
+++ src/encoder.rs
@@ -1688,7 +1688,12 @@ fn encode_partition_bottomup(seq: &Sequence, fi: &FrameInvariants, fs: &mut Fram
         let mv = mode_decision.mv;
         let skip = mode_decision.skip;
         let mut cdef_coded = cw.bc.cdef_coded;
-        let (tx_size, tx_type) = (mode_decision.tx_size, mode_decision.tx_type);
+        //let (tx_size, tx_type) = (mode_decision.tx_size, mode_decision.tx_type);
+        let (tx_size, tx_type) =
+            rdo_tx_size_type(seq, fi, fs, cw, bsize, bo, mode_luma, ref_frame, mv, skip);
+
+        assert!(tx_size == mode_decision.tx_size);
+        assert!(tx_type == mode_decision.tx_type);
 
         rd_cost = mode_decision.rd_cost + cost;
 
@@ -1765,7 +1770,12 @@ fn encode_partition_bottomup(seq: &Sequence, fi: &FrameInvariants, fs: &mut Fram
             let mv = best_decision.mv;
             let skip = best_decision.skip;
             let mut cdef_coded = cw.bc.cdef_coded;
-            let (tx_size, tx_type) = (best_decision.tx_size, best_decision.tx_type);
+            //let (tx_size, tx_type) = (best_decision.tx_size, best_decision.tx_type);
+            let (tx_size, tx_type) =
+                rdo_tx_size_type(seq, fi, fs, cw, bsize, bo, mode_luma, ref_frame, mv, skip);
+
+            assert!(tx_size == best_decision.tx_size);
+            assert!(tx_type == best_decision.tx_type);
 
             let mut mv_stack = Vec::new();
             let mode_context = cw.find_mvrefs(bo, ref_frame, &mut mv_stack, bsize, false);
@@ -1853,7 +1863,12 @@ fn encode_partition_topdown(seq: &Sequence, fi: &FrameInvariants, fs: &mut Frame
             let ref_frame = part_decision.ref_frame;
             let mv = part_decision.mv;
             let mut cdef_coded = cw.bc.cdef_coded;
-            let (tx_size, tx_type) = (part_decision.tx_size, part_decision.tx_type);
+            //let (tx_size, tx_type) = (part_decision.tx_size, part_decision.tx_type);
+            let (tx_size, tx_type) =
+                rdo_tx_size_type(seq, fi, fs, cw, bsize, bo, mode_luma, ref_frame, mv, skip);
+
+            assert!(tx_size == part_decision.tx_size);
+            assert!(tx_type == part_decision.tx_type);
 
             let mut mv_stack = Vec::new();
             let mode_context = cw.find_mvrefs(bo, ref_frame, &mut mv_stack, bsize, false);
