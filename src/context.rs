8607dcd32b2751671c6fc38878da6fbecbf1b9bd
Yushin Cho <ycho@mozilla.com>
2018-11-06 15:01:37 -0800

    Add conditions to use write_partition() for horz and vert partitions

--- src/context.rs
+++ src/context.rs
@@ -1905,6 +1905,7 @@ impl ContextWriter {
     if has_rows && has_cols {
       symbol_with_update!(self, w, p as u32, partition_cdf);
     } else if !has_rows && has_cols {
+      assert!(p == PartitionType::PARTITION_SPLIT || p == PartitionType::PARTITION_HORZ);
       assert!(bsize > BlockSize::BLOCK_8X8);
       let mut cdf = [0u16; 2];
       ContextWriter::partition_gather_vert_alike(
@@ -1914,6 +1915,7 @@ impl ContextWriter {
       );
       w.symbol((p == PartitionType::PARTITION_SPLIT) as u32, &cdf);
     } else {
+      assert!(p == PartitionType::PARTITION_SPLIT || p == PartitionType::PARTITION_VERT);
       assert!(bsize > BlockSize::BLOCK_8X8);
       let mut cdf = [0u16; 2];
       ContextWriter::partition_gather_horz_alike(
