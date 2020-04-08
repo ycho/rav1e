1a7218ef6b5ae7c1629551ee83e5fc3466489c0b
Yushin Cho <ycho@mozilla.com>
2020-04-08 13:22:03 -0700

    just last work

--- src/transform/inverse.rs
+++ src/transform/inverse.rs
@@ -1647,13 +1647,23 @@ pub(crate) mod rust {
         );
       }
       txfm_fn(&temp_in, &mut temp_out, range);
-      for (temp, out) in temp_out
+      /*for (temp, out) in temp_out
         .iter()
-        .zip(output.rows_iter_mut().map(|row| &mut row[c]).take(height))
+      .zip(output.rows_iter_mut().map(|row| &mut row[c]).take(height))
       {
         let v: i32 = (*out).as_();
         let v = clamp(v + round_shift(*temp, 4), 0, (1 << bd) - 1);
         *out = T::cast_from(v);
+      }*/
+
+      for y in 0..height {
+        let temp = temp_out[y];
+        unsafe {
+        let out = output.data_ptr_mut().add(output.plane_cfg.stride * y + c);
+        let v: i32 = (*out).as_();
+        let v = clamp(v + round_shift(temp, 4), 0, (1 << bd) - 1);
+        *out = T::cast_from(v);
+        }
       }
     }
   }
